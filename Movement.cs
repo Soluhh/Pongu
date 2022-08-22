668081c8334be4f5884761f53923b3d57604ea79
Soluhh <111203670+Soluhh@users.noreply.github.com>
2022-08-22 12:25:35 -0700

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1;
+
+    void Update()
+    {
+        float y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(0, y, 0) * Time.deltaTime * speed;
+    }
+}
