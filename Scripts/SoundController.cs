0394f42240f874479e8590f51ee0c2cd78c4666e
Soluhh <111203670+Soluhh@users.noreply.github.com>
2022-08-22 13:06:24 -0700

    Add files via upload

---
+++ Scripts/SoundController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource wallSound;
+    public AudioSource racketSound;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "RacketPlayer1" || collision.gameObject.name == "RacketPlayer2")
+        {
+            this.racketSound.Play();
+            Debug.Log("Racket Hit");
+        }
+        else
+            this.wallSound.Play();
+    }
+}
